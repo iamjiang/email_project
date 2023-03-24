1ab2a844800d21f60c726aedc8f408b42c0b1a30
EC2 Default User <ec2-user@ip-172-16-74-113.ec2.internal>
2023-03-24 21:15:23 +0000

    add codes to download transformers model into ominiAI repository

---
+++ download_transformers/centillion_download.py
@@ -0,0 +1,80 @@
+import os
+import json
+import random
+import string
+import requests
+from datetime import datetime
+from configparser import ConfigParser
+import argparse
+from tqdm import tqdm
+ 
+BASE = "BASE"
+PROJECT = "PROJECT"
+TOKEN = "TOKEN"
+PROXIES = {
+    "https": "http://proxy.jpmchase.net:8443",
+    "http": "http://proxy.jpmchase.net:8443"
+}
+#It works from linux not from vdi
+def get_token():
+    path=os.getenv("OMNI_USER_PKG_FRS_REQ").split("/")[1:-1]
+    path.append("tokens.ini")
+    path = "/" + "/".join(path)
+    cfg = ConfigParser()
+    cfg.read(path)
+    access_token = cfg.get("TokensSection","token")
+    return access_token
+###
+def download_artifact(api, model_name, model_version,):
+    headers = {"Authorization": "Bearer {}".format(api[TOKEN]), "content-type": "application/json"}
+    url_string = api[BASE] + "/v1/model-repo/model-artifacts/{}/{}/{}".format(api[PROJECT], model_name, model_version)
+    response = requests.get(url=url_string, headers=headers, verify=True)
+    print(response.url)
+ 
+    status = response.status_code
+    print("status code " , status)
+    print("content = ", response.content)
+    for item in json.loads(response.content):
+        print("Downloading file {} ".format(item["fileName"]))
+        download_url_string = api[BASE] + "/v1/model-repo/model-artifacts/download/{}/{}/{}/{}".format(api[PROJECT], model_name, model_version, item["fileName"])
+        download_headers = {"Authorization": "Bearer {}".format(api[TOKEN])}
+        print("start download time ********************",datetime.now().strftime("%Y%m%d-%H:%M:%S"))
+ 
+        download_response = requests.get(url=download_url_string, headers=headers, verify=True, stream=True)
+        downloaded_file = item["fileName"]#"/tmp/downloaded_"+datetime.now().strftime("%Y%m%d_%H%M%S")+item["fileName"]
+        if download_response.status_code == 200:
+            temp_file = open(downloaded_file, "wb")
+            count = 0
+            for chunk in tqdm(download_response.iter_content(chunk_size=4194304)):
+                count = count + 1
+                temp_file.write(chunk)
+                #print ("chunk {} done".format(count))
+            print("File {} downloaded as {} ".format(item["fileName"], downloaded_file))
+        else:
+               print("          ===============> download error....", download_response.status_code, download_response.content)
+ 
+        print("End Download time ",datetime.now().strftime("%Y%m%d-%H:%M:%S"))
+    return status
+###
+def main():
+    try:
+        del os.environ['http_proxy']
+        del os.environ['https_proxy']
+        del os.environ['no_proxy']
+    except:
+        print("env already setup. No action taken.......")
+    token = get_token()
+    api = {
+        BASE : "https://omni-modelrepo-api.prod.aws.jpmchase.net",
+        PROJECT : "Public Models TRN",
+        TOKEN : token
+    }
+    parser = argparse.ArgumentParser(description='get model_name and version')
+    parser.add_argument('--model_name' , help='model_name')
+    parser.add_argument('--model_version', help='model_version')
+  
+    args = parser.parse_args()
+    download_artifact(api, args.model_name, args.model_version)
+######
+main()
+
---
+++ download_transformers/run.sh
@@ -0,0 +1,15 @@
+python centillion_download.py --model_name roberta-large_0 --model_version v1.0
+python centillion_download.py --model_name roberta-base --model_version v2.0
+python centillion_download.py --model_name finbert_2 --model_version v1.0
+python centillion_download.py --model_name microsoft-deberta-base --model_version v1.0
+python centillion_download.py --model_name allenai-longformer-base-4096 --model_version v1.0
+python centillion_download.py --model_name longformer-base-4096-0 --model_version v1.0
+python centillion_download.py --model_name longformer-large-4096_0 --model_version v1.0
+
+python CTO_centillion.py --model_name finGloVe_50dim --model_version 1.0
+python CTO_centillion.py --model_name finGloVe_100dim --model_version 1.0
+python CTO_centillion.py --model_name finGloVe_300dim --model_version 1.0
+
+# tar -xvzf finGloVe_100d.tar.gz  -C ./Glove_w2v/
+tar -xvzf longformer-large-4096.tar.zip  
+
