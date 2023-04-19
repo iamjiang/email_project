716e4f4a3392c25f5355228452fe9f3d9ff709a5
EC2 Default User <ec2-user@ip-172-16-55-32.ec2.internal>
2023-04-19 14:29:00 +0000

    updates

--- cnn/.ipynb_checkpoints/run-checkpoint.sh
+++ cnn/.ipynb_checkpoints/run-checkpoint.sh
@@ -13,12 +13,11 @@ python train.py  \
 
 export CUDA_VISIBLE_DEVICES=2
 python test.py  \
---batch_size 32 \
---val_set ./datasets/sample_val.csv \
---test_set ./datasets/sample_test.csv \
+--batch_size 1024 \
 --train_undersampling \
 --train_negative_positive_ratio 5 \
 --val_min_recall 0.95 \
 --device cuda
+
 
 
